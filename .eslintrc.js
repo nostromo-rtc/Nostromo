937e9ec1ef5c8be7824f60f24774ddc42ee2eb31
SgAkErRu <sulmpx60@yandex.ru>
2021-09-12 05:33:31 +0400

    Добавлен eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./**/tsconfig.json"
+    },
+    plugins: [
+        "@typescript-eslint"
+    ],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    ],
+    rules: {
+        "@typescript-eslint/promise-function-async": "error",
+        "@typescript-eslint/array-type": "error",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-misused-promises": [
+            "error",
+            {
+                "checksVoidReturn": false
+            }
+        ]
+    }
+};
